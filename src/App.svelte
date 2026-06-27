211919f9aa144de32d02f46b67b9cc1ed909b001
Axiaaa <lucie.camerlynck@hotmail.com>
2026-06-27 03:33:48 +0200

    chores: some cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,17 @@
 <script>
-  import LetterLogic from './lib/Main.svelte';
+  import Main from './lib/Main.svelte';
 </script>
 
 
 <section>
-<h1>
-  <span>W</span>
-  <span>o</span>
-  <span>r</span>
-  <span style='color: #b59f3b'>d</span>
-  <span style='color: #538d4e'>l</span>
-  <span>e</span>
-</h1>
+  <h1>
+    <span>W</span>
+    <span>o</span>
+    <span>r</span>
+    <span style='color: #b59f3b'>d</span>
+    <span style='color: #538d4e'>l</span>
+    <span>e</span>
+  </h1>
 
-  <LetterLogic />
+  <Main />
 </section>
